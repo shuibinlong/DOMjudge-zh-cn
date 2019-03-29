a306fe1c2c3af6f2e356e3add804fa83cb7c658e
strawberry <dragon60066@163.com>
2019-03-29 18:14:15 +0800

    Update menu.php

--- team/menu.php
+++ team/menu.php
@@ -2,6 +2,99 @@
 $fdata = calcFreezeData($cdata);
 $started = checkrole('jury') || $fdata['started'];
 ?>
+
+
+<?php
+$langdata = $DB->q('KEYTABLE SELECT langid AS ARRAYKEY, name, extensions, require_entry_point, entry_point_description
+FROM language WHERE allow_submit = 1');
+
+$probdata = $DB->q('TABLE SELECT probid, shortname, name FROM problem
+INNER JOIN contestproblem USING (probid)
+WHERE cid = %i AND allow_submit = 1
+ORDER BY shortname', $cid);
+?>
+
+<script>
+<?php putgetMainExtension($langdata);?>
+</script>
+
+<?php
+$maxfiles = dbconfig_get('sourcefiles_limit', 100);
+
+$probs = array();
+$probs[''] = '请选择一个题目';
+foreach ($probdata as $probinfo) {
+    $probs[$probinfo['probid']]=$probinfo['shortname'] . ' - ' .$probinfo['name'];
+}
+
+$langs = array();
+$langs[''] = '请选择一种编程语言';
+foreach ($langdata as $langid => $langdata) {
+    $langs[$langid] = $langdata['name'];
+}
+?>
+
+<div class="modal fade" id="SubmitForm"> 
+   <div class="modal-dialog"> 
+    <div class="modal-content"> 
+     <div class="modal-header"> 
+      <h2 class="modal-title">提交代码</h2> 
+      <button type="button" class="close" data-dismiss="modal">&times;</button> 
+     </div>
+    <form action="upload.php" method="post" enctype="multipart/form-data" onsubmit="return checkUploadForm();">
+     <div class="modal-body"> 
+        <div class="form-group">
+          <label for="maincode">代码源文件：</label>
+          <input type="file" class="form-control-file" name="code[]" id="maincode" required <?=($maxfiles > 1 ? 'multiple': '')?> />
+          <!-- <div class="custom-file">
+            <input type="file" class="custom-file-input" name="code[]" id="maincode" required <?=($maxfiles > 1 ? 'multiple': '')?> />
+            <label for="maincode" class="custom-file-label">选择文件</label>
+          </div> -->
+        </div>
+        <div class="form-group">
+          <label for="probid">题目ID：</label>
+          <select class="custom-select" name="probid" id="probid" required>
+<?php
+    foreach ($probs as $probid => $probname) {
+        print '      <option value="' .specialchars($probid). '">' . specialchars($probname) . "</option>\n";
+    }
+?>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="langid">语言：</label>
+          <select class="custom-select" name="langid" id="langid" required>
+<?php
+    foreach ($langs as $langid => $langname) {
+        print '      <option value="' .specialchars($langid). '">' . specialchars($langname) . "</option>\n";
+    }
+?>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="entry_point" id="entry_point_text">程序入口点:</label>
+          <input type="text" class="form-control" name="entry_point" id="entry_point" aria-describedby="entrypointhelp">
+          <small id="entrypointhelp" class="form-text text-muted">请输入代码的程序入口点（C 和 C++ 可空，Java 请输入其入口类名）</small>
+        </div>
+        <script type="text/javascript">initFileUploads(<?=$maxfiles?>);</script>
+     </div> 
+     <div class="modal-footer"> 
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+      <button type="submit" name="submit" class="btn btn-primary"><span class="octicon octicon-cloud-upload"></span> 提交代码</button> 
+     </div>
+     </form>
+    </div>
+   </div>
+  </div>
+
+  <script>
+    function ShowSubmitDialog() {
+      $("#SubmitForm").modal("show");
+    }
+  </script>
+
+
+
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <a class="navbar-brand hidden-sm-down" href="./">BIT Online Judge</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menuDefault" aria-controls="menuDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,7 +130,7 @@ $started = checkrole('jury') || $fdata['started'];
       </div>
 
 <?php if ($started): ?>
-      <div id="submitbut"><a class="nav-link justify-content-center" href="submit.php"><button type="button" class="btn btn-success btn-sm"><span class="octicon octicon-cloud-upload"></span> 提交代码</button></a></div>
+      <div id="submitbut"><a class="nav-link justify-content-center" onclick="ShowSubmitDialog()"><button type="button" class="btn btn-success btn-sm"><span class="octicon octicon-cloud-upload"></span> 提交代码</button></a></div>
 <?php else: ?>
       <div id="submitbut"><a class="nav-link justify-content-center"><button type="button" class="btn btn-success btn-sm disabled"><span class="octicon octicon-cloud-upload"></span> 提交代码</button></a></div>
 <?php endif; ?>
